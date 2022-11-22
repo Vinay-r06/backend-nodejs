c1fc12b4e621239352a8617e271bb04bcc240fec
Vinay R <74782469+Vinay-r06@users.noreply.github.com>
2022-11-22 22:36:16 +0530

    Delete task6ReqNode.js

--- task6ReqNode.js
+++
@@ -1,8 +0,0 @@
-const http =require('http');  
-
- const server=http.createServer((req,res)=>{
- console.log('vinay');                                      // open browser localhost:3000...u will not get anything because we are not return any html page...after opening browser see vs code terminal..there will be lot of output...because u  (req) console that
-
-  });
-
- server.listen(4000) 
